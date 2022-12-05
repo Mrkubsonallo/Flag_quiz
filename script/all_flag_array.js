ab2d07969ca02e221e5c65f04161e776eeff05a0
Mrkubsonallo <jakubek.c25@gmail.com>
2022-12-05 13:14:07 +0100

    Dzialajace w kolejnosci alfabetycznej

--- script/all_flag_array.js
+++ script/all_flag_array.js
@@ -1,3 +1,22 @@
+let flag_div = document.createElement('div')
+let next_btn_cre = document.createElement('button')
+let sub_btn_cre = document.createElement('button')
+let input_cre = document.createElement('input')
+flag_div.setAttribute('id', 'flag')
+next_btn_cre.setAttribute('id', 'next_btn')
+sub_btn_cre.setAttribute('id', 'sub_btn')
+input_cre.setAttribute('id', 'text_flag')
+next_btn_cre.textContent = 'Następna flaga'
+sub_btn_cre.textContent = 'Sprawdź'
+document.body.innerHTML = ""
+document.body.appendChild(flag_div)
+document.body.appendChild(next_btn_cre)
+document.body.appendChild(sub_btn_cre)
+document.body.appendChild(input_cre)
+
+let i = 0
+let correct = 0
+
 const country = []
 
 country[0] = "Afganistan"
@@ -197,8 +216,56 @@ country[193] = "Zimbabwe"
 country[194] = "Zjednoczone Emiraty Arabskie"
 
 const flag = document.querySelector("#flag")
+const next_btn = document.querySelector("#next_btn")
+const sub_btn = document.querySelector("#sub_btn")
+const input = document.querySelector("#text_flag")
 
-for(i=0; i<country.length; i++)
-{
-    flag.innerHTML += '<img src="img/'+country[i]+'.svg" alt="flaga" style="width: 300px; height: 200px; margin: 5px;">'
-}
+
+if(flag.children.length == 0)
+    flag.innerHTML = '<img src="img/'+country[i]+'.svg" alt="flaga" style="width: 300px; height: 200px; margin: 5px;">'
+
+next_btn.addEventListener('click', ()=>{
+    if(i<country.length-1)
+    {
+        i++
+        flag.innerHTML = '<img src="img/'+country[i]+'.svg" alt="flaga" style="width: 300px; height: 200px; margin: 5px;">'
+    }
+    else
+        document.body.innerHTML = "Twój wynik to: " + correct
+})
+
+sub_btn.addEventListener('click', ()=>{
+    if(input.value == country[i])
+    {
+        if(i<country.length-1)
+        {
+            i++
+            correct++
+            flag.innerHTML = '<img src="img/'+country[i]+'.svg" alt="flaga" style="width: 300px; height: 200px; margin: 5px;">'
+            input.value = ""
+        }
+        else
+        {
+            correct++
+            document.body.innerHTML = "Twój wynik to: " + correct
+        }
+    } 
+})
+
+input.addEventListener('change', ()=>{
+    if(input.value == country[i])
+    {
+        if(i<country.length-1)
+        {
+            i++
+            correct++
+            flag.innerHTML = '<img src="img/'+country[i]+'.svg" alt="flaga" style="width: 300px; height: 200px; margin: 5px;">'
+            input.value = ""
+        }
+        else
+        {
+            correct++
+            document.body.innerHTML = "Twój wynik to: " + correct
+        }
+    }
+})
